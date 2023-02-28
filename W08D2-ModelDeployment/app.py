e73cc35d4d99c53a93be54624ad6e9db2be4c35d
Sabyasachi Dasgupta <sabyadg.softbio@gmail.com>
2023-02-28 12:51:21 -0500

    Add new lectures

---
+++ W08D2-ModelDeployment/app.py
@@ -0,0 +1,51 @@
+from flask import render_template, request, jsonify,Flask
+import flask
+import numpy as np
+import traceback #allows you to send error to user
+import pickle
+import pandas as pd
+
+
+# App definition
+app = Flask(__name__)
+
+# importing models
+with open('regressor.pkl', 'rb') as f:
+   regressor = pickle.load (f)
+
+with open('model_columns.pkl', 'rb') as f:
+   model_columns = pickle.load (f)
+
+#webpage
+
+@app.route('/')
+def welcome():
+   return "Welcome! Use this Flask App for Boston Housing Price Prediction"
+
+@app.route('/predict', methods=['POST','GET'])
+def predict():
+
+   if flask.request.method == 'GET':
+       return "Prediction page. Try using post with params to get specific prediction."
+
+   if flask.request.method == 'POST':
+       try:
+           json_ = request.json # '_' since 'json' is a special word
+           print(json_)
+           query_ = pd.get_dummies(pd.DataFrame(json_))
+           query = query_.reindex(columns = model_columns, fill_value= 0)
+           prediction = list(regressor.predict(query))
+
+           return jsonify({
+               "prediction":str(prediction)
+           })
+
+       except:
+           return jsonify({
+               "trace": traceback.format_exc()
+               })
+
+
+
+if __name__ == "__main__":
+   app.run()
---
+++ W08D2-ModelDeployment/test.py
@@ -0,0 +1,41 @@
+## Python test file for flask to test locally
+import requests as r
+import pandas as pd
+import json
+#https://www.postman.com/downloads/
+
+base_url = 'http://127.0.0.1:5000/' #base url local host
+
+json_data = [
+    {
+    "CRIM" : 0.63,
+    "ZN" : 10.0,
+    "INDUS" : 2.31,
+    "CHAS" : 0.0,
+    "NOX" : 0.0538,
+    "RM" : 6.575,
+    "AGE" : 65.2,
+    "DIS" : 4.0900,
+    "RAD" : 1.0,
+    "TAX" : 296.0,
+    "PTRATIO" : 15.3,
+    "B" : 369.90,
+    "LSTAT": 0
+    }
+]
+
+
+
+# Get Response
+# response = r.get(base_url)
+response = r.post(base_url + "predict", json = json_data)
+
+
+if response.status_code == 200:
+    print('...')
+    print('request successful')
+    print('...')
+    print(response.json())
+else:
+    print(response.json())
+    print('request failed')
